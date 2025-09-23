ab48093e31644291227c5e00031a6b74d3917c27
ThiagoAciole <thiagoaciole7@gmail.com>
2025-09-23 08:20:05 -0300

    feat: Compress Profiler Img

--- src/modules/Experience/styled.ts
+++ src/modules/Experience/styled.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Line = styled.div`
   height: 30px;
-  width: 1px;
+  width: 2px;
   border-radius: 10px;
-  border: 1px solid gray;
   margin-left: 18px;
+  background-color: var(--gray-6);
 `;
